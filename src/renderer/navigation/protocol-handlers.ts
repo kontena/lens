1f1694b4d7ef6cc79d493fe6cfcbfc88e3c45cc9
Sebastian Malton <sebastian@malton.name>
2021-03-02 14:04:32 -0500

    add common app routes to the protocol renderer router
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/navigation/protocol-handlers.ts
+++ src/renderer/navigation/protocol-handlers.ts
@@ -1,10 +1,31 @@
+import { addClusterURL } from "../components/+add-cluster";
+import { clusterSettingsURL } from "../components/+cluster-settings";
+import { extensionsURL } from "../components/+extensions";
+import { landingURL } from "../components/+landing-page";
+import { preferencesURL } from "../components/+preferences";
+import { clusterViewURL } from "../components/cluster-manager/cluster-view.route";
 import { LensProtocolRouterRenderer } from "../protocol-handler/router";
 import { navigate } from "./helpers";
 
 export function bindProtocolHandlers() {
-  const lprr = LensProtocolRouterRenderer.getInstance<LensProtocolRouterRenderer>();
-
-  lprr.addInternalHandler("/preferences", () => {
-    navigate("/preferences");
-  });
+  LensProtocolRouterRenderer
+    .getInstance<LensProtocolRouterRenderer>()
+    .addInternalHandler("/preferences", () => {
+      navigate(preferencesURL());
+    })
+    .addInternalHandler("/landing", () => {
+      navigate(landingURL());
+    })
+    .addInternalHandler("/cluster", () => {
+      navigate(addClusterURL());
+    })
+    .addInternalHandler("/cluster/:clusterId", ({ pathname: { clusterId } }) => {
+      navigate(clusterViewURL({ params: { clusterId } }));
+    })
+    .addInternalHandler("/cluster/:clusterId/settings", ({ pathname: { clusterId } }) => {
+      navigate(clusterSettingsURL({ params: { clusterId } }));
+    })
+    .addInternalHandler("/extensions", () => {
+      navigate(extensionsURL());
+    });
 }
